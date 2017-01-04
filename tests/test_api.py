774cfdcf7116734b3f94e1ed7248cc63770c0645
steven-murray <steven.murray@curtin.edu.au>
2017-01-04 09:17:35 +0800

    Added heaps of tests against analytic integrals and some checking input/output

---
+++ tests/test_api.py
@@ -0,0 +1,60 @@
+'''
+This module provides some tests of the API of hankel.
+
+That is, it tests stuff like whether one can input scalars/arrays, and whether the outputs of functions have the correct
+number of entries etc.
+'''
+
+import numpy as np
+import inspect
+import os
+LOCATION = "/".join(os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))).split("/")[:-1])
+import sys
+sys.path.insert(0, LOCATION)
+
+from nose.tools import raises
+
+from hankel import HankelTransform, SymmetricFourierTransform
+
+@raises(ValueError)
+def test_array_N():
+    ht = HankelTransform(N=np.linspace(100,200,10))
+
+@raises(ValueError)
+def test_array_h():
+    ht = HankelTransform(h=np.logspace(-3, 0, 10))
+
+@raises(ValueError)
+def test_array_nu():
+    ht = HankelTransform(nu=np.linspace(0,1,2))
+
+
+def test_k_array():
+    k = np.logspace(-3,3,10)
+    ht = HankelTransform(N=50)
+    res = ht.transform(lambda x : 1./x, k, False)
+    assert len(res) == 10
+
+def test_k_scalar():
+    k = 1
+    ht = HankelTransform(N=50)
+    res = ht.transform(lambda x : 1./x, k, False)
+    assert np.isscalar(res)
+
+def test_ret_err():
+    ht = HankelTransform(N=50)
+    res,err = ht.integrate(lambda x : 1./x, True)
+
+def test_ret_cumsum():
+    ht = HankelTransform(N=50)
+    res,cumsum = ht.integrate(lambda x : 1./x, False,True)
+
+def test_ret_err_and_cumsum():
+    ht = HankelTransform(N=50)
+    res,err,cumsum = ht.integrate(lambda x : 1./x, True,True)
+
+def test_equivalence_of_integrate_and_transform():
+    ht = HankelTransform(N=50)
+    int = ht.integrate(lambda x : 1,False)
+    tr = ht.transform(lambda x : 1./x, ret_err=False)
+    assert int == tr
---
+++ tests/test_fourier.py
@@ -0,0 +1,79 @@
+'''
+This module provides some tests for the Symmetric Fourier Transforms.
+
+Note that most of the tests in here are mirrored in test_known_integrals, in which k is set to 1.
+
+There is a corresponding notebook in devel/ that runs each of these functions through a grid of N and h,
+showing the pattern of accuracy. This could be useful for finding the correct numbers to choose for these
+for other unknown functions.
+'''
+
+import numpy as np
+import inspect
+import os
+LOCATION = "/".join(os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))).split("/")[:-1])
+import sys
+sys.path.insert(0, LOCATION)
+
+
+
+from scipy.special import k0, gamma, gammainc, gammaincc
+
+gammainc_ = lambda a,x : gamma(a)*gammainc(a,x)
+gammaincc_ = lambda a,x : gamma(a)*gammaincc(a,x)
+
+from hankel import SymmetricFourierTransform
+
+class TestSymmetricFourierTransforms(object):
+
+    def powerlaw(self,s,ndim,k,N,h):
+        """
+        Test f(r) = 1/r, nu=0
+        """
+
+
+        ht = SymmetricFourierTransform(ndim=ndim, N=N, h=h)
+        ans = ht.transform(lambda x: x**s, k, False, False)
+
+        nu = ndim/2. - 1
+        s += nu
+        if nu-s <= 0 and (nu-s)%2==0:
+            raise Exception("Can't have a negative integer for gamma")
+
+        anl = (2*np.pi)**(ndim/2.) * 2**(s+1) * gamma(0.5*(2+nu+s))/k**(s+2)/gamma(0.5*(nu-s))/ k**nu
+
+        print "Numerical Result: ", ans, " (required %s)"%anl
+        assert np.isclose(ans,anl,rtol=1e-3)
+
+    def test_powerlaw(self):
+        trials = [#[-2, 4, 0.01, 300, 10 ** -3.2],
+        #           [-2, 4, 1, 300, 10 ** -3.2],
+        #           [-2, 4, 10.0, 300, 10 ** -3.2],
+        #           [-2, 6, 0.01, 200, 10 ** -2.],
+        #           [-2, 6, 1, 200, 10 ** -2.],
+        #           [-2, 6, 10.0, 200, 10 ** -2.],
+                  [-1, 2, 0.01, 50, 0.05],
+                  [-1, 2, 1, 50, 0.05],
+                  [-1, 2, 10.0, 50, 0.05],
+                  [-1, 4, 0.01, 50, 0.05],
+                  [-1, 4, 1, 50, 0.05],
+                  [-1, 4, 10.0, 50, 0.05],
+                  [-1, 6, 0.01, 50, 0.05],
+                  [-1, 6, 1, 50, 0.05],
+                  [-1, 6, 10.0, 50, 0.05],
+                  [1, 2, 0.01, 150, 10 ** -1.5],
+                  [1, 2, 1, 150, 10 ** -1.5],
+                  [1, 2, 10.0, 150, 10 ** -1.5],
+                  [1, 3, 0.01, 150, 10 ** -1.5],
+                  [1, 3, 1, 150, 10 ** -1.5],
+                  [1, 3, 10.0, 150, 10 ** -1.5],
+                  [-1, 3, 0.01, 150, 10 ** -1.5],
+                  [-1, 3, 1, 150, 10 ** -1.5],
+                  [-1, 3, 10.0, 150, 10 ** -1.5],
+
+            # [1, 6, 0.01, 50, 0.05],
+                  # [1, 6, 1, 50, 0.05],
+                  # [1, 6, 10.0, 50, 0.05],
+                  ]
+        for s, nu, k, N, h in trials:
+            yield self.powerlaw, s, nu, k, N, h
