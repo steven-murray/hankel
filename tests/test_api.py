3ee12b1dcedd99be9b549c4c56a400926832563f
Steven Murray <steven.murray@curtin.edu.au>
2018-12-11 15:29:13 -0700

    formatting

--- tests/test_api.py
+++ tests/test_api.py
@@ -5,56 +5,66 @@ That is, it tests stuff like whether one can input scalars/arrays, and whether t
 number of entries etc.
 '''
 
-import numpy as np
 import inspect
+import numpy as np
 import os
+
 LOCATION = "/".join(os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))).split("/")[:-1])
 import sys
+
 sys.path.insert(0, LOCATION)
 
 from nose.tools import raises
 
 from hankel import HankelTransform, SymmetricFourierTransform
 
+
 @raises(ValueError)
 def test_array_N():
-    ht = HankelTransform(N=np.linspace(100,200,10))
+    ht = HankelTransform(N=np.linspace(100, 200, 10))
+
 
 @raises(TypeError)
 def test_array_h():
     ht = HankelTransform(h=np.logspace(-3, 0, 10))
 
+
 @raises(ValueError)
 def test_array_nu():
-    ht = HankelTransform(nu=np.linspace(0,1,2))
+    ht = HankelTransform(nu=np.linspace(0, 1, 2))
 
 
 def test_k_array():
-    k = np.logspace(-3,3,10)
+    k = np.logspace(-3, 3, 10)
     ht = HankelTransform(N=50)
-    res = ht.transform(lambda x : 1./x, k, False)
+    res = ht.transform(lambda x: 1. / x, k, False)
     assert len(res) == 10
+
 
 def test_k_scalar():
     k = 1
     ht = HankelTransform(N=50)
-    res = ht.transform(lambda x : 1./x, k, False)
+    res = ht.transform(lambda x: 1. / x, k, False)
     assert np.isscalar(res)
+
 
 def test_ret_err():
     ht = HankelTransform(N=50)
-    res,err = ht.integrate(lambda x : 1./x, True)
+    res, err = ht.integrate(lambda x: 1. / x, True)
+
 
 def test_ret_cumsum():
     ht = HankelTransform(N=50)
-    res,cumsum = ht.integrate(lambda x : 1./x, False,True)
+    res, cumsum = ht.integrate(lambda x: 1. / x, False, True)
+
 
 def test_ret_err_and_cumsum():
     ht = HankelTransform(N=50)
-    res,err,cumsum = ht.integrate(lambda x : 1./x, True,True)
+    res, err, cumsum = ht.integrate(lambda x: 1. / x, True, True)
+
 
 def test_equivalence_of_integrate_and_transform():
     ht = HankelTransform(N=50)
-    int = ht.integrate(lambda x : 1,False)
-    tr = ht.transform(lambda x : 1./x, ret_err=False)
+    int = ht.integrate(lambda x: 1, False)
+    tr = ht.transform(lambda x: 1. / x, ret_err=False)
     assert int == tr
